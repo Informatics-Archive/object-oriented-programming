9312e11eb5715c9b59704e3953d138a5aac8e78b
Revyan Aldy Wilaga <revyanaldywilaga@gmail.com>
2026-04-08 15:23:22 +0700

    drop section-0 because it same as section-1

--- oracle-academy/section-1/eclipse/MySecondProject/src/mysecondproject/HelloMain.java
+++ oracle-academy/section-1/eclipse/MySecondProject/src/mysecondproject/HelloMain.java
@@ -3,7 +3,6 @@ package mysecondproject;
 public class HelloMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello again world");
 	}
 
